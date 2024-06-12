5c8582e1f42ebd7272cbdbfbd18d53d9a4d60461
nessed <ali.abid44444@gmail.com>
2024-06-12 15:16:54 +0500

    ui update

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,18 @@
 import Image from "next/image";
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="text-9xl font-extralight text-white">Hello</div>
+    <main className="flex size flex-col h-full w-full items-center justify-between">
+      <div className="flex flex-col pt-10 px-10 pb-20 pr-20">
+        <div className="font-extralight text-9xl text-white pb-20">
+          welcome to your one-stop productivity app
+        </div>
+        <div className="text-white text-5xl">productivity begins here.</div>
+      </div>
+      <div className="flex flex-col justify-end py-10">
+        <div class=" border border-white text-white py-8 px-16 rounded-full text-xl hover:bg-white hover:text-gray-800 transition-colors duration-300 cursor-pointer">
+          <div className="text-2xl">Get Started</div>
+        </div>
+      </div>
     </main>
   );
 }
