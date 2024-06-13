3a8f9d1e8f01fee242dbcaf77b1bc0f5c9ecf2bf
nessed <ali.abid44444@gmail.com>
2024-06-13 15:06:21 +0500

    added aos animations

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,6 +2,7 @@ import { Inter, Roboto, Lato } from "next/font/google";
 import "./globals.css";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
+ 
 
 const inter = Inter({ subsets: ["latin"] });
 const roboto = Roboto({ subsets: ["latin"], weight: ["100", "300", "400"] });
@@ -13,6 +14,8 @@ export const metadata = {
 };
 
 export default function RootLayout({ children }) {
+
+
   return (
     <html lang="en">
       <body className={lato.className.thin}>{children}</body>
