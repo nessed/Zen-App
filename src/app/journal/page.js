e5e24a3b6cd439311c3e91e5be8899930ae397fe
nessed <ali.abid44444@gmail.com>
2024-06-24 04:43:43 +0500

    added basic interface

--- src/app/journal/page.js
+++ src/app/journal/page.js
@@ -1,5 +1,24 @@
 import React from "react";
 
-export default function journal() {
-  return <div>page</div>;
+export default function Journal() {
+  return (
+    <div className="flex min-h-screen">
+      <div className="w-1/3 flex">
+        <div className="flex-center text-center">Journal</div>
+      </div>
+      <div className="w-2/3  p-4 flex flex-col justify-between bg-opacity-40 bg-blue-400">
+        <div className="h-full">
+          <h2 className="text-2xl font-bold text-white mb-4"> </h2>
+          <p className="text-white ">Hello</p>
+        </div>
+        <div className="mt-4 ">
+          <input
+            type="text"
+            className="w-full p-2 border border-gray-300 rounded"
+            placeholder="Enter your text here"
+          />
+        </div>
+      </div>
+    </div>
+  );
 }
